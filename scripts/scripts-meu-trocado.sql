b177886f388df13e5d674ffdcd0def4b5bc26efd
Murilo Jatobá <murilo_75517@trt5.jus.br>
2023-09-23 19:50:42 -0300

    Ajuste no arquivo de scripts.

--- scripts/scripts-meu-trocado.sql
+++ scripts/scripts-meu-trocado.sql
@@ -6,10 +6,6 @@ from tb_conta c
 ;
 
 
-insert into tb_conta (ds_nome, in_principal, id_usuario, id_usuario_ult_alteracao)
-values ('Banco do Brasil', true, 2, 2)
-;
-
 select * 
 from tb_categoria c 
 order by 2
@@ -36,6 +32,21 @@ where id_conta = :idConta
 and dt_referencia = :dtRef
 ;
 
+
+select c.id_categoria, c.ds_nome, sum(l.nr_valor) valor, 'L' tipo_valor
+from tb_categoria c
+left join tb_lancamento l using (id_categoria)
+where l.dt_referencia = :dtRef
+group by c.id_categoria
+
+union all 
+
+select c.id_categoria, c.ds_nome, avg(l.nr_valor) valor, 'M' tipo_valor
+from tb_categoria c
+left join tb_lancamento l using (id_categoria)
+where dt_referencia between (:dtRef::date - interval '1 year')::date and :dtRef
+group by c.id_categoria
+;
 
 -- Agosto
 insert into tb_lancamento (cd_tipo, id_conta, dt_referencia, id_categoria, nr_valor, ds_descricao, id_usuario_ult_alteracao)
@@ -74,5 +85,7 @@ and dt_referencia = :dtRef
 
 
 select (date_trunc('year','2023-08-01'::date) + interval '11 month')
+
+
 
 
